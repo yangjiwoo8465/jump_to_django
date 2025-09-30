410eaa79a09c610ebdd50bf48d153829bf55785b
양지우 <yangjiwoo8465@gmail.com>
2025-09-30 18:38:22 +0900

    postres DB 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,22 @@
+import environ
+
 from .base import *
 
 ALLOWED_HOSTS = ['3.39.35.168']
 STATIC_ROOT = BASE_DIR / 'pybo/static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}
